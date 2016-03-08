32716c39453997125e23b3ff2f4b732b79edf91e
なかのひと <nullblog@github.io>
2016-03-09 00:01:18 +0900

    add 2016-03-09

---
+++ work/src/detail/personal/20160308.php
@@ -0,0 +1,31 @@
+<?php
+$meta = array(
+  'description' => 'トリビアはちゃんと検証をして実際の映像を見せてくれるから好きだったですよ。',
+  'keywords' => 'トリビアの泉,タモリ,ランナー',
+  'title' =>  'こういうのめっちゃ良いじゃん！',
+  'date' => '2016-03-08'
+);
+?>
+<?php include('src/common/_head.php'); ?>
+<div class="row">
+  <div class="col-xs-12">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h3 class="panel-title text-center"><?php echo $meta['title']; ?></h3>
+      </div>
+      <div class="panel-body">
+        <p><strong>そりゃあタモさんだって泣くよ</strong></p>
+        <p>凄く感動したから貼り付け</p>
+        <p><iframe src="https://www.youtube.com/embed/XHXeI0_vtg0?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allowfullscreen style="max-width: 100%;"></iframe></p>
+        <p><iframe src="https://www.youtube.com/embed/ggM6diL3QR8?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allowfullscreen style="max-width: 100%;"></iframe></p>
+        <p>トリビアはちゃんと検証をして</p>
+        <p>実際の映像を見せてくれるから好きだったですよ。</p>
+        <p>中学の理科で習った</p>
+        <p>「予想」「実験」「検証」の流れを</p>
+        <p>ちゃんと守ってくれる</p>
+        <p>とっても素敵な番組でした。</p>
+      </div>
+    </div>
+  </div>
+</div>
+<?php include('src/common/_foot.php'); ?>
